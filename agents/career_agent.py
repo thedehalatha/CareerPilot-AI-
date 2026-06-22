301762e84c80f020f90923b28ff401607d341bb8
thedehalatha <rdehalatha24@gmail.com>
2026-06-22 21:39:41 +0530

    Initial CareerPilot AI agent setup

---
+++ agents/career_agent.py
@@ -0,0 +1,22 @@
+from google.adk.agents import Agent
+
+
+career_agent = Agent(
+    name="CareerPilot",
+    model="gemini-2.0-flash",
+    description="AI career assistant",
+    instruction="""
+
+You are CareerPilot AI.
+
+Your job is to help students with:
+
+1. Career guidance
+2. Skill gap analysis
+3. Learning roadmap
+4. Interview preparation
+
+Give clear and practical answers.
+
+"""
+)
---
+++ test_agent.py
@@ -0,0 +1,48 @@
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
+
+from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
+from google.genai import types
+
+from agents.career_agent import career_agent
+
+
+async def main():
+
+    session_service = InMemorySessionService()
+
+    runner = Runner(
+        agent=career_agent,
+        app_name="CareerPilot",
+        session_service=session_service
+    )
+
+    user_id = "student1"
+
+    session = await session_service.create_session(
+        app_name="CareerPilot",
+        user_id=user_id
+    )
+
+    message = types.Content(
+        role="user",
+        parts=[
+            types.Part(
+                text="I want to become an AI Engineer. What should I learn?"
+            )
+        ]
+    )
+
+    async for response in runner.run_async(
+        user_id=user_id,
+        session_id=session.id,
+        new_message=message
+    ):
+        if response.content:
+            print(response.content.parts[0].text)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
